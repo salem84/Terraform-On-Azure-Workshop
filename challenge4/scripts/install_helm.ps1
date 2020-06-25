7bcc7519e682f86ef0169fda79b4259c1a6eafae
Giorgio Lasala <giorgio.lasala@eng.it>
2020-06-25 23:52:29 +0200

    scripts challenge 4

--- challenge4/scripts/install_helm.ps1
+++
@@ -1,3 +0,0 @@
-# From Azure Shell
-param([Parameter(Mandatory)][string] $aksHostName)
-helm upgrade --install tailwindtraders-web ./Deploy/helm/web -f ./Deploy/helm/gvalues.yaml -f ./Deploy/helm/values.b2c.yaml  --set ingress.hosts={$aksHostName}  --set ingress.protocol=http --set ingress.tls[0].hosts={$aksHostName} --set image.repository=giorgiolasala/tailwindtradersweb --set image.tag=latest
